44ab732b64d95128c5297d5f90cad508faaec860
Kevyn Lima <kevynlimasilva1@gmail.com>
2020-03-26 10:29:13 -0300

    components1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { ContatosComponent } from './contatos/contatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    ServicosComponent,
+    EmpresaComponent,
+    ContatosComponent
   ],
   imports: [
     BrowserModule
